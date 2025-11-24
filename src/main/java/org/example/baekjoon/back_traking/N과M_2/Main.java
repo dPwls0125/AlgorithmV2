41cf77d1bab63377da5c3f380b250454dd08fa20
kimYejin <kimphoby0125@gmail.com>
2025-11-24 23:36:35 +0900

    백트래킹_N과M(2) : https://www.acmicpc.net/problem/15650

---
+++ src/main/java/org/example/baekjoon/back_traking/N과M_2/Main.java
@@ -0,0 +1,55 @@
+package org.example.baekjoon.back_traking.N과M_2;
+
+import java.io.*;
+
+public class Main {
+
+    // 1~N까지 자연수 중에서 중복 없이 M개를 고른 수열
+    // 오름차순
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static int[] answer = new int[10];
+    private static boolean[] visited = new boolean[10];
+
+    static int n;
+
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+
+        String[] inputs = br.readLine().split(" ");
+        n = Integer.parseInt(inputs[0]);
+        m = Integer.parseInt(inputs[1]);
+
+        bt(0);
+        bw.close();
+
+    }
+
+
+    public static void bt(final int location) throws IOException {
+        if (location == m){
+            for(int i=0; i<m; i++){
+                bw.write(Integer.toString(answer[i]) +  " ");
+            }
+            bw.write("\n");
+            bw.flush();
+        }
+
+        for(int i=1; i<=n; i++){
+            if(!visited[i]){
+
+                if (  location > 0 && answer[location-1] > i){
+                    continue;
+                }
+
+                visited[i] = true;
+                answer[location] = i;
+                bt(location+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
