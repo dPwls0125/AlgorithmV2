69501c780498c12c6e1d3c1254ddcc69e2803e09
kimYejin <kimphoby0125@gmail.com>
2025-12-22 22:12:32 +0900

    외판원 순회2(백트래킹) : https://www.acmicpc.net/problem/10971

--- src/main/java/org/example/baekjoon/back_traking/외판원_순회2/Main.java
+++ src/main/java/org/example/baekjoon/back_traking/외판원_순회2/Main.java
@@ -15,19 +15,18 @@ public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static int n;
-    public static int[][] arr;
-
-    public static int Min = Integer.MAX_VALUE;
+    static int n;
+    static int[][] arr;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
 
         // Read Input
         String input = br.readLine();
         n = Integer.parseInt(input);
         arr = new int[n+1][n+1];
+        visited = new boolean[n+1];
 
-        boolean[] visited = new boolean[n+1];
-        int[] cost = new int[n+1];
 
         for(int i=0; i<n; i++){
             String[] inputs = br.readLine().split(" ");
@@ -38,27 +37,33 @@ public class Main {
 
         // Logic
         for(int start=0; start<n; start++){ // Starting Point
-            backTrack(start,start,visited,cost,0);
+            visited[start] = true;
+            backTrack(start, start,1,0 );
+            visited[start] = false;
         }
 
         // Write Output
+        bw.write(String.valueOf(min));
+        bw.flush();
+        bw.close();
 
 
     }
 
-    private static void backTrack(int start_location, int location, boolean[] visited, int[] cost, int num){
+    private static void backTrack(int start_location, int location, int count, int sum){
 
-        visited[location] = true;
-        num += 1;
-
-        if(num > n && arr[location][start_location] > 0){
-            cost[start_location] = Math.min()
+        if(count == n){
+            if(arr[location][start_location] > 0){
+                min = Math.min(min, sum + arr[location][start_location]);
+            }
+            return;
         }
 
         for(int i=0; i<n; i++){
-            if(!visited[i] && arr[location][i] != 0 ){
-                cost[start_location] += arr[location][i];
-                backTrack(start_location,i,visited,cost,num);
+            if(!visited[i] && arr[location][i] > 0 ){
+                visited[i] = true;
+                backTrack(start_location, i,count +1, sum + arr[location][i]);
+                visited[i] = false;
             }
         }
     }
